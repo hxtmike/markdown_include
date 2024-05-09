39f7ce024876b21258343a719d6def35bea49c18
Xiaotian Han <xiaotianhan@Xiaotians-Mac-mini.local>
2024-05-09 23:15:05 +0100

    refactor: clean the whole script

--- markdown_include.py
+++ markdown_include.py
@@ -1,11 +1,13 @@
 #!/usr/bin/env python3
 
+from typing import Optional
 import re
 import sys
 
-def markdown_include(inputFile: str, outputFile: str=None) -> None: 
+
+def markdown_include(inputFile: str, outputFile: Optional[str] = None) -> None:
     # define default output filename
-    if outputFile == None:
+    if outputFile is None:
         indexPeriod = inputFile.rfind('.')
 
         if indexPeriod == -1:
@@ -16,15 +18,13 @@ def markdown_include(inputFile: str, outputFile: str=None) -> None:
             suffix = inputFile[indexPeriod:]
 
         outputFile = prefix + '_mdincl_output' + suffix
-        # print(outputFile)
-    
+
     # fine the relative dir of the inputFile
     indexSlash = inputFile.rfind("/")
     if indexSlash == -1:
         directory = "./"
     else:
         directory = inputFile[:indexSlash+1]
-    # print(directory)
 
     try:
         with open(inputFile, 'r') as file:
@@ -33,34 +33,35 @@ def markdown_include(inputFile: str, outputFile: str=None) -> None:
         print('Unexcepted Error: input file read error')
         sys.exit()
 
-    # print(content)
-    # print("#" * 50)
     includes = re.findall(r'<!-- include \((.*)\) -->', content)
 
     for include in includes:
         try:
-            # print(directory+include)
             with open(directory + include, 'r') as file:
                 includeContent = file.read()
-                # print(includeContent)
-                # print("#" * 50)
         except:
             print('Unexcepted Error: included file cannot be found')
             sys.exit()
-        
-        content = content.replace('<!-- include ({}) -->'.format(include), includeContent)
-    
+
+        content = content.replace(
+            '<!-- include ({}) -->'.format(include),
+            includeContent
+        )
+
     with open(outputFile, 'w') as file:
         file.write(content)
 
-if __name__ == '__main__':
+def main():
     lenarg = len(sys.argv)
-    if lenarg < 2 or lenarg > 3 :
+    if lenarg < 2 or lenarg > 3:
         print("Unexcepted Error: the argument should be '<input file> <output file>'")
         sys.exit()
-    
+
     if lenarg == 2:
         markdown_include(sys.argv[1])
-    
+
     if lenarg == 3:
         markdown_include(sys.argv[1], sys.argv[2])
+
+if __name__ == '__main__':
+    main()
